626227f986e939ac88626591feb026e46d1346ef
MalekFarag <43182938+MalekFarag@users.noreply.github.com>
2019-03-04 13:24:26 -0500

    added transitions via JS

--- js/main.js
+++ js/main.js
@@ -7,11 +7,31 @@
 		if (!audio) { return; }
 
 		audio.currentTime = 0;
-		audio.loop = true;
+		//audio.loop = true;
 		audio.play();
+
+		let key = document.querySelector(`div[data-key='${e.keyCode}']`);
+		key.classList.add('playing')
+	}
+
+	function removePlayingClass(e) {
+		//removing css class in order to transition back
+		
+		if (e.propertyName !== 'transform') {
+			return;
+		} else {
+			console.log('Transition Ended.')
+		}
+	e.currentTarget.classList.remove('playing')
 	}
 
 	window.addEventListener('keydown', playDrumSound);
+
+	const keys = Array.from(document.querySelectorAll('.key'));
+
+
+
+	keys.forEach(key => key.addEventListener('transitionend', removePlayingClass))
 		}
 
 )();
